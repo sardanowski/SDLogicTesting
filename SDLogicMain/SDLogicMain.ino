f5bed9c9b10fc065d1cd44d8a41d7769d6c0c99a
dnwing <dnwing@mtu.edu>
2018-12-05 11:46:56 -0500

    Update SDLogicMain.ino
    Defining variables

--- SDLogicMain/SDLogicMain.ino
+++ SDLogicMain/SDLogicMain.ino
@@ -1,5 +1,20 @@
+//display pins
+#define TFT_DC 9
+#define TFT_CS 10
+#define TFT_MOSI 11
+#define TFT_CLK 13
+
+//Pin expander pins
+#define SDA A4
+#define SCL A5
+
+
 void setup() {
   // put your setup code here, to run once:
+  //DISPLAY()
+  //DEFINE Default pinouts
+  //initialize SPI AND I2C 
+  
 
 }
 
